20cca4f5a445a5641738f2f8a6340489eafee0ec
David Brown <davidb@codeaurora.org>
2010-10-10 23:34:20 -0700

    FAT: Fix warning
    fs/fat/dir.c:43: warning: 'de' may be used uninitialized in this function
    
    The complexity of the code flow makes this seem possible.  Initialize
    the value to NULL to eliminate the compiler warning.  This only masks
    the warning, since if the value were unused, there would still be a
    NULL pointer.
    
    Change-Id: I9fc36abace09409853b63e0997328b75ce703769
    Signed-off-by: David Brown <davidb@codeaurora.org>

--- fs/fat/dir.c
+++ fs/fat/dir.c
@@ -343,7 +343,7 @@ int fat_search_long(struct inode *inode, const unsigned char *name,
 	struct super_block *sb = inode->i_sb;
 	struct msdos_sb_info *sbi = MSDOS_SB(sb);
 	struct buffer_head *bh = NULL;
-	struct msdos_dir_entry *de;
+	struct msdos_dir_entry *de = NULL;
 	struct nls_table *nls_disk = sbi->nls_disk;
 	unsigned char nr_slots;
 	wchar_t bufuname[14];
@@ -468,7 +468,7 @@ static int __fat_readdir(struct inode *inode, struct file *filp, void *dirent,
 	struct super_block *sb = inode->i_sb;
 	struct msdos_sb_info *sbi = MSDOS_SB(sb);
 	struct buffer_head *bh;
-	struct msdos_dir_entry *de;
+	struct msdos_dir_entry *de = NULL;
 	struct nls_table *nls_disk = sbi->nls_disk;
 	unsigned char nr_slots;
 	wchar_t bufuname[14];
@@ -887,7 +887,7 @@ EXPORT_SYMBOL_GPL(fat_get_dotdot_entry);
 int fat_dir_empty(struct inode *dir)
 {
 	struct buffer_head *bh;
-	struct msdos_dir_entry *de;
+	struct msdos_dir_entry *de = NULL;
 	loff_t cpos;
 	int result = 0;
 
@@ -913,7 +913,7 @@ EXPORT_SYMBOL_GPL(fat_dir_empty);
 int fat_subdirs(struct inode *dir)
 {
 	struct buffer_head *bh;
-	struct msdos_dir_entry *de;
+	struct msdos_dir_entry *de = NULL;
 	loff_t cpos;
 	int count = 0;
 
@@ -1240,7 +1240,7 @@ int fat_add_entries(struct inode *dir, void *slots, int nr_slots,
 	struct super_block *sb = dir->i_sb;
 	struct msdos_sb_info *sbi = MSDOS_SB(sb);
 	struct buffer_head *bh, *prev, *bhs[3]; /* 32*slots (672bytes) */
-	struct msdos_dir_entry *uninitialized_var(de);
+	struct msdos_dir_entry *de = NULL;
 	int err, free_slots, i, nr_bhs;
 	loff_t pos, i_pos;
 
